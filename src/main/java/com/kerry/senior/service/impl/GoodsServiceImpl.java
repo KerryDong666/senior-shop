faeee1402ebc80c9d50cfeb9c3189449974bfc2c
Dc520134 <dc_1419@163.com>
2018-04-16 20:18:27 +0800

    补充提交商品Service

---
+++ src/main/java/com/kerry/senior/service/impl/GoodsServiceImpl.java
@@ -0,0 +1,61 @@
+package com.kerry.senior.service.impl;
+
+import com.kerry.senior.domain.SeckillGoods;
+import com.kerry.senior.exception.GlobalException;
+import com.kerry.senior.exception.ParamMissingException;
+import com.kerry.senior.mapper.GoodsMapper;
+import com.kerry.senior.result.CodeMsg;
+import com.kerry.senior.service.GoodsService;
+import com.kerry.senior.vo.GoodsVo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author CP_dongchuan
+ */
+@Service
+public class GoodsServiceImpl implements GoodsService {
+    private static final Logger logger = LoggerFactory.getLogger(GoodsServiceImpl.class);
+    @Autowired
+    private GoodsMapper goodsMapper;
+    @Override
+    public List<GoodsVo> listGoodsVo() {
+        List<GoodsVo> goodsVos = goodsMapper.listGoodsVo();
+        logger.info("查询商品信息成功, msg = {}", goodsVos);
+        return goodsVos;
+    }
+
+    /**
+     * 根据商品id查询商品详情
+     * @param goodsId
+     * @return
+     */
+    @Override
+    public GoodsVo getGoodsVoByGoodsId(Long goodsId) {
+        if (goodsId == null) {
+            throw new ParamMissingException(CodeMsg.PARAM_ERROR);
+        }
+        GoodsVo goodsVo = goodsMapper.queryGoodsVoByGoodsId(goodsId);
+        logger.info("查询产品信息成功, msg = {}", goodsVo);
+        return goodsVo;
+    }
+
+    /**
+     * 减少库存
+     */
+    @Override
+    public void reduceStock(GoodsVo goods) {
+        SeckillGoods seckillGoods = new SeckillGoods();
+        seckillGoods.setGoodsId(goods.getId());
+        int line = goodsMapper.reduceStock(seckillGoods);
+        if (line < 1) {
+            logger.error("更新库存信息失败, goodsId = {}", goods.getId());
+            throw new GlobalException(CodeMsg.SQL_ERROR);
+        }
+        logger.info("更新库存信息成功, goodsId = {}", goods.getId());
+    }
+}
